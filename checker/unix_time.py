3941d193a6e9b88cc4ff4f6838a20a812e90f748
OutlyingWest <alexeybuv7@gmail.com>
2023-02-14 13:19:13 +0300

    improvements in time module

--- checker/unix_time.py
+++ checker/unix_time.py
@@ -2,16 +2,26 @@ import datetime
 
 
 def main():
-    date_str = input('Enter time in format Y-m-d:')
-    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
-    get_unix_time_in_ms_utc(date_obj.year, date_obj.month, date_obj.day)
+    unix_time_ms = convert_to_unix_time_by_input()
+    print(unix_time_ms)
 
 
-def get_unix_time_in_ms_utc(year, month, day):
-    """ Returns time(ms) in unix format """
+def convert_to_unix_time_ms(year, month, day):
+    """ Returns time(ms) in unix format UTC """
     date_obj = datetime.datetime(year, month, day)
     unix_timestamp = int(date_obj.replace(tzinfo=datetime.timezone.utc).timestamp()) * 1000
     return unix_timestamp
+
+
+def convert_to_unix_time_by_input():
+    """
+    Allow to enter time in format Y-m-d.
+    Returns time(ms) in unix format UTC.
+    """
+    date_str = input('Enter time in format Y-m-d:')
+    date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+    unix_time = convert_to_unix_time_ms(date_obj.year, date_obj.month, date_obj.day)
+    return unix_time
 
 
 if __name__ == "__main__":
