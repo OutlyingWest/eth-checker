806d948d060b892b4bddcbfa21ada31aecb8f4ad
OutlyingWest <alexeybuv7@gmail.com>
2023-02-15 13:13:39 +0300

    linear regression module added

---
+++ checker/linear_regression.py
@@ -0,0 +1,7 @@
+import asyncio
+from sklearn.linear_model import LinearRegression
+from checker.binance_handler import BinanceGetDate
+from checker.unix_time import convert_date_to_unix_time_by_string
+
+
+
